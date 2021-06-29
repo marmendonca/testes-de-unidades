f2dd790edda7d1dbdc421b8806c2d93ea7bc0e2d
Marília Mendonça <marilianobre1@gmail.com>
2021-06-29 16:43:37 -0300

    Implementando os commands e fazendo seus respectivos testes.

---
+++ Store/Store.Domain/Commands/CreateOrderCommand.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Commands
+{
+    public class CreateOrderCommand : Notifiable, ICommand
+    {
+        public CreateOrderCommand()
+        {
+            Items = new List<CreateOrderItemCommand>();
+        }
+
+        public CreateOrderCommand(string customer, string zipCode, string promoCode, IList<CreateOrderItemCommand> items)
+        {
+            Customer = customer;
+            ZipCode = zipCode;
+            promoCode = PromoCode;
+            items = Items;
+        }
+
+        public string Customer { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string PromoCode { get; set; }
+
+        public IList<CreateOrderItemCommand> Items { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasLen(Customer, 11, "Customer", "Cliente inválido")
+                .HasLen(ZipCode, 8, "ZipCode", "Cep inválido")
+            );
+        }
+    }
+}
